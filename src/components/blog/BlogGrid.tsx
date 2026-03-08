e9f0e7bb2c9b9d40c1b39187c640ca4f78a0f574
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-08 13:51:05 +0100

    Update BlogGrid.tsx

--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -1,3 +1,17 @@
-import React from 'react';
+import type { Article } from "@/content/blog/articles";
+import { BlogCard } from "@/components/blog/BlogCard";
 
-export const BlogGrid = () => null;
+type BlogGridProps = {
+  articles: Article[];
+  locale: string;
+};
+
+export function BlogGrid({ articles, locale }: BlogGridProps) {
+  return (
+    <div className="grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3">
+      {articles.map((article) => (
+        <BlogCard key={article.id} article={article} locale={locale} />
+      ))}
+    </div>
+  );
+}
